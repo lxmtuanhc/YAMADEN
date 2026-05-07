6d372b4d40e424868c7fc2561d0e22d6cc39f98a
Tuan <tuan@email.com>
2026-05-07 14:46:11 +0900

    Fix mobile login layout stability

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yamaden-support-v7";
+const CACHE_NAME = "yamaden-support-v9";
 const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
