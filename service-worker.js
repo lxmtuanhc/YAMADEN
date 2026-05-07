d85292841cc07704013aef0e2f2a8d21860bc484
Tuan <tuan@email.com>
2026-05-07 15:13:54 +0900

    Respect mobile safe area on login

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yamaden-support-v16";
+const CACHE_NAME = "yamaden-support-v19";
 const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
